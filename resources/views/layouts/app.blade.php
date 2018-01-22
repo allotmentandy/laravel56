99cefe68fe34d17ec2ee01c3a18c2bdd0a48098c
andy <andy.derkacz@gmail.com>
2018-01-22 09:12:00 +0000

    added andy.css, included in the app.blade.php layout and created a readme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,9 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-    <link rel="stylesheet" href="/css/app.css">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/andy.css') }}" rel="stylesheet">
+
 
 </head>
 
